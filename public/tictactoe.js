1abe087752d0240136aff1ae918a23ad84f548dc
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2019-04-15 12:24:01 -0400

    Initialize game board

--- public/tictactoe.js
+++ public/tictactoe.js
@@ -1,1 +1,64 @@
-console.log("Hello world")
+console.log('Hello world')
+
+const baseState = function () {
+  return [null, null, null, null, null, null, null, null, null];
+};
+
+let historyState = [];
+let currentState, turn;
+
+let resetBoard = function() {
+  console.log("Called resetBoard");
+  currentState = baseState();
+  historyState = [];
+  turn = 'X';
+  updateBoard();
+}
+
+let updateBoard = function (state) {
+  console.log("Called updateBoard");
+  let board = document.querySelector('#game-board');
+  console.log(board);
+  if (!board) {
+    console.log("Didn't find board.");
+    return;
+  }
+  board.innerHTML = buildBoard(state || currentState);
+}
+
+let buildBoard = function (state) {
+  let rows = '<table><tbody>';
+  rows += buildSquares(state);
+  rows += '</tbody></table><p><button id="play-again">Play Again</button></p>';
+  return rows;
+}
+
+let buildSquares = function (state) {
+  let rows = '';
+  state.forEach(function (square, id) {
+    let value = square ? square : '';
+    let selected = square ? ' aria-pressed="true"' : '';
+    let disabled = square ? ' disabled' : '';
+    if (isFirstInRow(id)) {
+      rows += '<tr>'
+    }
+    rows += (
+      '<td><button class="game-square" data-id="' + id + '"'
+      + selected + disabled +'>' + value + '</button></td>'
+    );
+    if (isLastInRow(id)) {
+      rows += '</tr>';
+    }
+  });
+  return rows;
+};
+
+let isFirstInRow = function (id) {
+  return ((id + 1) % 3 == 1);
+}
+
+let isLastInRow = function (id) {
+  return ((id + 1) % 3 == 0);
+}
+
+resetBoard();
